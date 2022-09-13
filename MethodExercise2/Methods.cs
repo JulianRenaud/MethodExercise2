f8a204ad00e1f8afc59af3c357e0bed14c5c9298
JulianRenaud <midway6220@gmail.com>
2022-09-13 11:31:56 -0500

    Fixed divide by 0 error

--- MethodExercise2/Methods.cs
+++ MethodExercise2/Methods.cs
@@ -61,7 +61,13 @@ namespace MethodExercise2
         public static double Divide(double num1, double num2)
         {
 
-            return num1 / num2;
+           
+            if (num2 == 0)
+            {
+                return 0;
+            }
+            else
+                return num1 / num2;
 
 
 
